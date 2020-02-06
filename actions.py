f19fdc6f3ed12ba9394a9250899441b4fbf5ce2c
harshal27 <harshaljain27@gmail.com>
2020-02-06 16:34:12 +0530

    Harshal: adding meeting fetching api

--- actions.py
+++ actions.py
@@ -32,6 +32,7 @@ import logging
 import pprint, json
 from rasa_sdk.events import SlotSet
 from timefhuman import timefhuman
+import requests
 
 logger = logging.getLogger(__name__)
 
@@ -43,8 +44,31 @@ class ActionFetchMeetings(Action):
   def run(self, dispatcher, tracker, domain):
     message = tracker.latest_message['text']
     try:
-      msg = timefhuman(message)
-      msg = msg.strftime("%m/%d/%Y, %H:%M:%S")
+      date = timefhuman(message).strftime("%Y-%m-%d")
+      logger.warn(date)
+      user_uuid = "A-Ub9LsfZfR5ZS12E1oGmA"
+
+      base_url = "https://light.jntesting.net/api/portal/calendar/users?"
+      params = "api_params[calendar_start_date]=" + str(date) +  "&api_params[calendar_end_date]=" + str(date) + "&api_params[user_uuid]=" + user_uuid + "&api_params[current_location_uuid]=FIdWeyXx5XgB-RSrmo1jAA"
+
+      url = base_url + params
+      headers = {
+        'X-Mobile-User-UUID': 'djiPw8pI-wUNBPvD9FfgGQ',
+        'Authorization': 'Bearer acd997f37ba67c52892c72f59cbf2eaff8c38367535f166034ef846997049f1a'
+      }
+
+      response = requests.get(url, headers=headers).json()
+      meetings = response['data']['items'][0]['calendar']['meetings']
+      meetings_for_date = meetings.get(date)
+      logger.warn(meetings_for_date)
+      msg = ""
+
+      if meetings_for_date is not None:
+        for y in meetings_for_date:
+          msg = " meeting: " + y["meeting_with"] + " " + y["location"] + " \n " + msg
+      else:
+        msg = "No meetings found !!"
+    
     except:
       msg = "Please try to frame the meeting date in another format"
 
